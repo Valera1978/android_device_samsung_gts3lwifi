8df328cd9f255c02aadcbe14a2a4d003d0ff4786
Valera1978 <valera1978@tut.by>
2019-12-06 13:43:32 +0300

    update vndk/hudl stuff

--- device.mk
+++ device.mk
@@ -226,8 +226,7 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0
 
 # IPC router config
 PRODUCT_COPY_FILES += \
